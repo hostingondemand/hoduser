edff842910c9afb762249e51ac4ddf05ea53d698
Mark Smit <m.smit@hostingondemand.nl>
2016-12-01 15:24:30 +0100

    minor style fixes

--- aspect/apiAuthorize.php
+++ aspect/apiAuthorize.php
@@ -3,7 +3,6 @@
     use lib\annotation\baseAspect;
 
     class ApiAuthorize extends BaseAspect{
-
         var $init;
         function onMethodPreCall($parameters, $data)
         {
@@ -13,14 +12,11 @@
                     $this->auth->setUser($user);
                     $this->init=true;
                 }
-
-
+            
                 if(!$this->auth->isAuthorized("attribute",$parameters[0],isset($parameters[1])?$parameters[1]:1)){
                     $this->event->raise("authorizationFail",array("parameters"=>$parameters,"data"=>$data));
                 }
-
         }
-
     }
 
 ?>
