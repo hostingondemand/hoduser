aa2df503d3e7d8af17256261e61cbb57133e0fcc
mark <m.smit@hostingondemand.nl>
2017-12-06 17:29:46 +0100

    minor fixes

--- lib/auth.php
+++ lib/auth.php
@@ -1,83 +1,107 @@
 <?php
-    namespace modules\hoduser\lib;
-    use hodphp\core\Lib;
 
-    class Auth extends Lib{
+namespace modules\hoduser\lib;
 
-        private $user;
+use hodphp\core\Lib;
 
-        function __construct()
-        {
-            $this->user=$this->service->user->getUserByHash($this->session->userHash);
-        }
+class Auth extends Lib
+{
 
-        function getField($field){
-            return $this->user->$field;
-        }
+    private $user;
 
-        function getUserId(){
-            if($this->user){
-                if($this->session->fakeUserId) {
-                    return $this->session->fakeUserId;
-                }else{
-                    return $this->user->id;
-                }
-            }
-            return 0;
-        }
+    function __construct()
+    {
+        $this->setup();
+    }
 
-        function getUserName(){
-            if($this->user) {
-                return $this->user->username;
-            } return "";
-        }
-
-        function isAuthenticated(){
-            return (bool)$this->user;
-        }
-
-        function isAuthorized($type,$key,$minLevel){
-
-            if($this->user){
-                $id=$this->user->id;
-            }else{
-                $id=0;
-            }
-            $level=$this->service->user->getLevel($id,$type,$key);
-            return $level >=$minLevel;
-        }
-
-        function setUser($user){
-            $this->user=$user;
-        }
-
-        function setupFakeSession($id){
-            $this->session->fakeUserId=$id;
-        }
-
-        function isInFakeSession(){
-            return $this->session->fakeUserId && $this->session->fakeUserId!=$this->user->id;
-        }
-
-        function stopFakeSession(){
-            $this->session->fakeUserId=false;
-        }
-
-        function getPreference($key)
-        {
-            if($this->user) {
-                $preference = $this->service->preference->getPreferenceByKey($this->user->id,$key);
-                if ($preference) {
-                    return $preference->value;
-                }
-            }
-                return false;
-        }
-
-        function setPreference($key,$value){
-            if($this->user) {
-                $this->service->preference->savePreference($this->user->id,$key,$value);
-            }
+    function setup($userId = false)
+    {
+        if(!$userId) {
+            $this->user = $this->service->user->getUserByHash($this->session->userHash);
+        }else {
+            $this->user = $this->service->user->getUserById($userId);
         }
     }
+
+    function getField($field)
+    {
+        return $this->user->$field;
+    }
+
+    function getUserId()
+    {
+        if ($this->user) {
+            if ($this->session->fakeUserId) {
+                return $this->session->fakeUserId;
+            } else {
+                return $this->user->id;
+            }
+        }
+        return 0;
+    }
+
+    function getUserName()
+    {
+        if ($this->user) {
+            return $this->user->username;
+        }
+        return "";
+    }
+
+    function isAuthenticated()
+    {
+        return (bool)$this->user;
+    }
+
+    function isAuthorized($type, $key, $minLevel)
+    {
+
+        if ($this->user) {
+            $id = $this->user->id;
+        } else {
+            $id = 0;
+        }
+        $level = $this->service->user->getLevel($id, $type, $key);
+        return $level >= $minLevel;
+    }
+
+    function setUser($user)
+    {
+        $this->user = $user;
+    }
+
+    function setupFakeSession($id)
+    {
+        $this->session->fakeUserId = $id;
+    }
+
+    function isInFakeSession()
+    {
+        return $this->session->fakeUserId && $this->session->fakeUserId != $this->user->id;
+    }
+
+    function stopFakeSession()
+    {
+        $this->session->fakeUserId = false;
+    }
+
+    function getPreference($key)
+    {
+        if ($this->user) {
+            $preference = $this->service->preference->getPreferenceByKey($this->user->id, $key);
+            if ($preference) {
+                return $preference->value;
+            }
+        }
+        return false;
+    }
+
+    function setPreference($key, $value)
+    {
+        if ($this->user) {
+            $this->service->preference->savePreference($this->user->id, $key, $value);
+        }
+    }
+}
+
 ?>
